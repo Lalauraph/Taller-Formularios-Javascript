b94dfb33f79b285fe5dbd4159ff11c599d9bf4ff
Lalauraph <laurapalacio900@gmail.com>
2023-09-28 17:59:31 -0500

    cambios js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-const form = document.querySelector('form');
+const render = () => {
+const formulario = document.querySelector("#registro");
+
 const resultado = document.querySelector('#resultado');
 
-form.addEventListener('submit', (event) => {
+formulario.addEventListener('submit', (event) => {
     event.preventDefault();
 
-    const nombre = form.nombre.value;
-    const apellido = form.apellido.value;
-    const edad = form.edad.value;
+    const nombre = formulario.nombre.value;
+    const apellido = formulario.apellido.value;
+    const edad = formulario.edad.value;
 
-    resultado.textContent = `El nombre de la persona es "${nombre}", su apellido es "${apellido}" y su edad es "${edad}" años.`;
+    resultado.textContent = "El nombre de la persona es " + nombre + ", su apellido es " + apellido + " y su edad es " + edad + " años.";
 });
+
+}
+
+window.onload = render;
